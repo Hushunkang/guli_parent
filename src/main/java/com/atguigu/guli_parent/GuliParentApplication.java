012f4b210ab22c1ee4043d8a93e26d26d91297d2
Hushunkang <2752962035@qq.com>
2020-04-04 23:48:41 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/guli_parent/GuliParentApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/guli_parent/GuliParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
