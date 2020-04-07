a20f2bfdcfdaed4f008220fc78716706c638b688
Hushunkang <2752962035@qq.com>
2020-04-07 21:04:03 +0800

    谷粒学院 基于@CrossOrigin注解解决跨域问题

--- service/service_oss/src/main/java/com/atguigu/ossservice/controller/OssController.java
+++ service/service_oss/src/main/java/com/atguigu/ossservice/controller/OssController.java
@@ -5,6 +5,7 @@ import com.atguigu.util.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(description = "OSS服务API")
 @RestController
 @RequestMapping("/ossservice/file")
+@CrossOrigin
 public class OssController {
 
     @Autowired
