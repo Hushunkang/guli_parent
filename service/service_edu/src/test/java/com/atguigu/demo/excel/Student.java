593a90da6fb0e170bd2ddf3aee67ec594a61e1c6
Hushunkang <2752962035@qq.com>
2020-04-08 02:32:46 +0800

    谷粒学院 easyExcel写操作简单的示例代码

---
+++ service/service_edu/src/test/java/com/atguigu/demo/excel/Student.java
@@ -0,0 +1,24 @@
+package com.atguigu.demo.excel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+/**
+ *
+ * easyExcel写操作简单的示例代码
+ *
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月08日 02时03分48秒
+ */
+@Data
+public class Student {
+
+    @ExcelProperty("学生编号")//设置Excel表头内容
+    private Integer studentNo;
+
+    @ExcelProperty("学生姓名")//设置Excel表头内容
+    private String studentName;
+
+}
---
+++ service/service_edu/src/test/java/com/atguigu/demo/excel/TestEasyExcel.java
@@ -0,0 +1,40 @@
+package com.atguigu.demo.excel;
+
+import com.alibaba.excel.EasyExcel;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月08日 02时08分42秒
+ */
+public class TestEasyExcel {
+
+    @Test
+    public void write(){
+        //设置给Excel文件写到哪儿去和文件名称
+        String filePath = "C:\\Users\\27529\\Desktop\\essential\\" + "grade8class6student.xlsx";
+        EasyExcel.write(filePath,Student.class).sheet("学生列表").doWrite(data());
+    }
+
+    private List<Student> data(){
+        List<Student> students = new ArrayList<>();
+        for(int i = 10;i > 0;i--){
+            Student student = new Student();
+            student.setStudentNo(i);
+            student.setStudentName("马云" + "_" + i);
+            students.add(student);
+        }
+        return students;
+    }
+
+    @Test
+    public void read(){
+
+    }
+
+}
