c7bc2e356cfda487a646c70af779b85fb6f2a9f0
Hushunkang <2752962035@qq.com>
2020-04-08 11:40:15 +0800

    谷粒学院 fix code(⊙︿⊙)

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -4,8 +4,11 @@ package com.atguigu.eduservice.controller;
 import com.atguigu.eduservice.service.EduSubjectService;
 import com.atguigu.util.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,7 +32,9 @@ public class EduSubjectController {
 
     //添加课程分类
     //获取上传过来的excel文件，然后读取excel文件内容
-    public R addSubject(MultipartFile file){
+    @ApiOperation(value = "添加课程分类")
+    @PostMapping("addSubject")
+    public R addSubject(@ApiParam(name = "file", value = "上传的文件") MultipartFile file){
         //上传过来的excel文件
         eduSubjectService.saveSubject(file,eduSubjectService);
         return R.ok();
