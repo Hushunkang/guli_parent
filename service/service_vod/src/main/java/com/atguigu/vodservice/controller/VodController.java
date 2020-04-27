83192e076ed8b4344cbd9a89e0d8a36ee93eb30e
Hushunkang <2752962035@qq.com>
2020-04-27 22:58:10 +0800

    根据阿里云视频点播服务为每一个视频生成的视频ID来获取视频播放凭证

--- service/service_vod/src/main/java/com/atguigu/vodservice/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vodservice/controller/VodController.java
@@ -1,7 +1,14 @@
 package com.atguigu.vodservice.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
+import com.atguigu.baseservice.exception.GuliException;
 import com.atguigu.util.R;
+import com.atguigu.util.ResultCode;
 import com.atguigu.vodservice.service.VodService;
+import com.atguigu.vodservice.util.ConstantVodUtils;
+import com.atguigu.vodservice.util.InitVodClientUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -38,6 +45,27 @@ public class VodController {
     public R removeVideo(@ApiParam(name = "videoSourceIds", value = "云端视频IDS", required = true) @PathVariable String videoSourceIds){
         vodService.removeVideo(videoSourceIds);
         return R.ok();
+    }
+
+    //根据阿里云视频点播服务为每一个视频生成的视频ID来获取视频播放凭证（补充一下，每一个云端视频的视频播放凭证是一样的，我测试发现是这样的）
+    @ApiOperation(value = "根据阿里云视频点播服务为每一个视频生成的视频ID来获取视频播放凭证")
+    @GetMapping("getVideoPlayAuth/{videoSourceId}")
+    public R getVideoPlayAuth(@ApiParam(name = "videoSourceId", value = "云端视频ID", required = true) @PathVariable String videoSourceId){
+        try {
+            //初始化一个vod的客户端
+            DefaultAcsClient client = InitVodClientUtils.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建获取视频播放凭证的request对象
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            //向视频播放凭证的request对象里面设置云端视频ID
+            request.setVideoId(videoSourceId);
+            //创建获取视频播放凭证的response对象
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            //从响应数据里面获取到视频播放凭证
+            String videoPlayAuth = response.getPlayAuth();
+            return R.ok().data("videoPlayAuth",videoPlayAuth);
+        }catch(Exception e) {
+            throw new GuliException(ResultCode.ERROR,"获取视频播放凭证失败，无法播放此视频(⊙︿⊙)");
+        }
     }
 
 }
